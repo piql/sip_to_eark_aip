6c06a0646922a0e3439b569ff3339d46f1662da2
Cameron <cameronlogin@gmail.com>
2021-11-17 12:15:53 +0000

    Convert to using pathlib

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import uuid
 import xml.etree.ElementTree as ET
 from datetime import datetime
+from pathlib import Path
 import metsrw
 
 
@@ -21,10 +22,10 @@ def get_arg(index):
 
 
 def validate_directories(sip_dir, output_dir):
-    if os.path.exists(sip_dir):
-        if os.path.isdir(sip_dir):
-            if os.path.exists(output_dir):
-                if os.path.isdir(output_dir):
+    if sip_dir.exists():
+        if sip_dir.is_dir():
+            if output_dir.exists():
+                if output_dir.is_dir():
                     return True
                 else:
                     print("Error: Output is not a directory")
@@ -40,20 +41,37 @@ def validate_directories(sip_dir, output_dir):
 def overwrite_and_create_directory(directory):
     """
     Deletes directory if it exists and (re)creates directory
-    :param str directory: path that needs to be (re)created
+    :param Path directory: path that needs to be (re)created
+    """
     """
     if os.path.isdir(directory):
         print("Overwriting '%s'" % directory)
         shutil.rmtree(directory)
     os.makedirs(directory)
+    """
+    if directory.is_dir():
+        print("Overwriting '%s'" % directory)
+        shutil.rmtree(directory)
+    try:
+        directory.mkdir(exist_ok=False)
+    except FileExistsError as e:
+        print(e)
+        print('Error with AIP overwrite')
+        sys.exit(1)
 
 
 def validate_representations_sequence(representations_path):
+    """
+
+    :param Path representations_path:
+    :return Boolean:
+    """
     # check sequence
     expected_sequence_number = 1
     previous_folder_name = '///'
 
-    for rep in sorted(os.listdir(representations_path)):
+    for rep in sorted(representations_path.iterdir()):
+        rep = str(rep.stem)
         folder_name = rep.rstrip('0123456789')
         try:
             sequence_number = int(rep[len(folder_name):])
@@ -72,28 +90,33 @@ def validate_representations_sequence(representations_path):
             return False
         else:
             expected_sequence_number += 1
-
     return True
 
 
 def copy_sip_to_aip(sip_path, aip_path):
+    """
+
+    :param Path sip_path:
+    :param Path aip_path:
+    :return:
+    """
     # copy required items in to submissions and copy in to AIP root if required
     items_to_copy = {'representations': False, 'metadata': True, 'schemas': True, 'documentation': True,
                      'METS.xml': False}
-    aip_submission_path = os.path.join(aip_path, "submission")
-    expected_sip_reps_path = os.path.join(sip_path, 'representations')
-    if os.path.isdir(expected_sip_reps_path):
+    aip_submission_path = aip_path / "submission"
+    expected_sip_reps_path = sip_path / 'representations'
+    if expected_sip_reps_path.is_dir():
         if validate_representations_sequence(expected_sip_reps_path):
-            os.makedirs(aip_submission_path)
+            aip_submission_path.mkdir()
             for item in items_to_copy.keys():
-                item_path = os.path.join(sip_path, item)
-                destination_path = os.path.join(aip_submission_path, item)
-                if os.path.isdir(item_path):
+                item_path = sip_path / item
+                destination_path = aip_submission_path / item
+                if item_path.is_dir():
                     shutil.copytree(item_path, destination_path)
                     # if True also copy item into root
                     if items_to_copy[item]:
-                        shutil.copytree(item_path, os.path.join(aip_path, item))
-                elif os.path.isfile(item_path):
+                        shutil.copytree(item_path, aip_path / item)
+                elif item_path.is_file():
                     # Will be METS.xml
                     shutil.copy2(item_path, destination_path)
                     # update_mets(destination_path)
@@ -109,19 +132,25 @@ def copy_sip_to_aip(sip_path, aip_path):
 
 
 def update_sip_representations(aip_path):
+    """
+
+    :param Path aip_path:
+    :return:
+    """
     # update representations and create preservation directories
-    output_submission_representations_path = os.path.join(aip_path, 'submission', "representations")
+    output_submission_representations_path = aip_path / 'submission' / "representations"
 
     print("Valid Rep Sequence:", validate_representations_sequence(output_submission_representations_path))
 
     if validate_representations_sequence(output_submission_representations_path):
-        for rep in sorted(os.listdir(output_submission_representations_path)):
+        for rep in sorted(output_submission_representations_path.iterdir()):
+            rep = str(rep.stem)
             rep_name = rep.rstrip('0123456789')
             rep_number = int(rep[len(rep_name):])
 
             # make preservation directory : rep1 -> rep01.1
-            preservation_rep_path = os.path.join(aip_path, "representations", "{}{:02}.1".format(rep_name, rep_number))
-            os.makedirs(os.path.join(preservation_rep_path, "data"))
+            preservation_rep_path = aip_path / "representations" / "{}{:02}.1".format(rep_name, rep_number)
+            (preservation_rep_path / "data").mkdir(parents=True)
             # create preservation rep METS.xml
             create_mets(preservation_rep_path)
 
@@ -136,8 +165,8 @@ def update_sip_representations(aip_path):
 
 
 def transform(sip_path, aip_path):
-    aip_name = os.path.basename(os.path.normpath(sip_path))
-    aip_path = os.path.join(aip_path, aip_name)
+    aip_name = sip_path.stem
+    aip_path = aip_path / aip_name
 
     overwrite_and_create_directory(aip_path)
 
@@ -155,10 +184,15 @@ def transform(sip_path, aip_path):
 
 
 def create_mets(path):
+    """
+
+    :param Path path:
+    :return:
+    """
     print("METS.xml written in: %s" % path)
     mets = metsrw.METSDocument()
     mets.append(create_fse(path, path))
-    mets.write(os.path.join(path, "METS.xml"), pretty_print=True)
+    mets.write(str(path / "METS.xml"), pretty_print=True)
 
 
 # NOT USED
@@ -191,35 +225,36 @@ def update_mets(mets_file):
 def create_fse(current_path, aip_root_path):
     """
     Recursive call
-    :param aip_root_path:
-    :param str current_path:
+    :param Path aip_root_path:
+    :param Path current_path:
     :return FSEntry fse:
     """
-    base_directory = os.path.basename(current_path)
+    base_directory = current_path.stem
     fse = metsrw.FSEntry(label=base_directory, type="Directory", file_uuid=str(uuid.uuid4()))
-
-    current_path_list = current_path[len(aip_root_path + '/'):].split('\\')
-    fse_use = current_path_list[0]
-    if fse_use == '':
+    relative_path = current_path.relative_to(aip_root_path)
+    try:
+        fse_use = relative_path.parts[0]
+    except IndexError:
         fse_use = 'original'
-    elif len(current_path_list)>1 and fse_use == 'representations':
-        fse_use += '/{}'.format(current_path_list[1])
+    else:
+        if len(relative_path.parts) > 1 and relative_path.parts[0] == 'representations':
+            fse_use = '{}/{}'.format(relative_path.parts[0], relative_path.parts[1])
 
     if "METS.xml" in os.listdir(current_path):
         print("METS found: %s" % current_path)
         file_fse = metsrw.FSEntry(use=fse_use, label="METS.xml",
-                                  path=os.path.join(current_path[len(aip_root_path):], "METS.xml"), type="Item",
+                                  path=str(relative_path / "METS.xml"), type="Item",
                                   file_uuid=str(uuid.uuid4()))
         fse.children.append(file_fse)
         return fse
 
     for item in os.listdir(current_path):
-        item_path = os.path.join(current_path, item)
-        if os.path.isdir(item_path):
+        item_path = current_path / item
+        if item_path.is_dir():
             fse.children.append(create_fse(item_path, aip_root_path))
-            pass
-        elif os.path.isfile(item_path):
-            file_fse = metsrw.FSEntry(use=base_directory, label=item, path=current_path, type="Item",
+        elif item_path.is_file():
+            print("item path: ", item_path, fse_use)
+            file_fse = metsrw.FSEntry(use=fse_use, label=item, path=str(relative_path / item), type="Item",
                                       file_uuid=str(uuid.uuid4()))
             fse.children.append(file_fse)
         else:
@@ -231,8 +266,8 @@ if __name__ == '__main__':
     numArgs = len(sys.argv)
 
     if numArgs == 3:
-        sip_directory = get_arg(1)
-        output_directory = get_arg(2)
+        sip_directory = Path(get_arg(1))
+        output_directory = Path(get_arg(2))
         if validate_directories(sip_directory, output_directory):
             # TODO: Validate SIP folder structure?
             transform(sip_directory, output_directory)
