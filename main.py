74f6e033ae4435e18589cd1a52f19a1678e7c93f
Cameron <cameronlogin@gmail.com>
2021-11-22 11:30:19 +0000

    generate AIP root METS.xml

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import hashlib
+import mimetypes
 import os
 import shutil
 import sys
@@ -5,6 +7,7 @@ import uuid
 import xml.etree.ElementTree as ET
 from datetime import datetime
 from pathlib import Path
+
 import metsrw
 
 
@@ -26,7 +29,14 @@ def validate_directories(sip_dir, output_dir):
         if sip_dir.is_dir():
             if output_dir.exists():
                 if output_dir.is_dir():
-                    return True
+                    sip_name = sip_dir.stem
+                    try:
+                        sip_uuid = str(sip_name[sip_name.index('uuid-') + len('uuid-'):])
+                        uuid_obj = uuid.UUID(sip_uuid, version=4)
+                    except ValueError:
+                        print("Error: SIP doesn't contain valid uuid4")
+                    else:
+                        return True
                 else:
                     print("Error: Output is not a directory")
             else:
@@ -79,11 +89,171 @@ def validate_representations_sequence(representations_path):
             return False
 
         if expected_sequence_number != sequence_number:
-            print("ERROR in SIP representations number sequence. Expected:", expected_sequence_number, "Got:", sequence_number)
+            print("ERROR in SIP rep number sequence. Expected:", expected_sequence_number, "Got:", sequence_number)
             return False
         else:
             expected_sequence_number += 1
     return True
+
+
+def new_uuid():
+    return 'new-uuid-' + str(uuid.uuid4())
+
+
+def update_all_mets_ids(mets_tree, id_updates):
+    """
+    :param ElementTree mets_tree:
+    :param dict id_updates:
+    :return:
+    """
+    root = mets_tree.getroot()
+
+    try:
+        if root.attrib['OBJID'] in id_updates:
+            root.attrib['OBJID'] = id_updates[root.attrib['OBJID']]
+        else:
+            print("Error: SIP OBJID doesn't match SIP uuid")
+            root.attrib['OBJID'] = new_uuid()
+    except ValueError:
+        print("Error: No OBJID in SIP METS.xml")
+
+    dmdsec_elements = root.findall('{http://www.loc.gov/METS/}dmdSec')
+    for dmdsec in dmdsec_elements:
+        new_id = new_uuid()
+        id_updates[dmdsec.attrib['ID']] = new_id
+        dmdsec.attrib['ID'] = new_id
+
+    amdsec_elements = root.findall('{http://www.loc.gov/METS/}amdSec')
+    for amdsec in amdsec_elements:
+        new_id = new_uuid()
+        id_updates[amdsec.attrib['ID']] = new_id
+        amdsec.attrib['ID'] = new_id
+
+    filesec_element = root.find('{http://www.loc.gov/METS/}fileSec')
+    filesec_element.attrib['ID'] = new_uuid()
+    for filegrp in filesec_element.findall('{http://www.loc.gov/METS/}fileGrp'):
+        new_id = new_uuid()
+        id_updates[filegrp.attrib['ID']] = new_id
+        filegrp.attrib['ID'] = new_id
+        for file_element in filegrp.findall('{http://www.loc.gov/METS/}file'):
+            new_id = new_uuid()
+            # id_updates[filegrp.attrib['ID']] = new_id
+            file_element.attrib['ID'] = new_id
+
+    structmap_element = root.find('{http://www.loc.gov/METS/}structMap')
+    structmap_element.attrib['ID'] = new_uuid()
+    root_div_element = structmap_element.find('{http://www.loc.gov/METS/}div')
+    root_div_element.attrib['LABEL'] = root.attrib['OBJID']
+    for div in root_div_element.findall('{http://www.loc.gov/METS/}div'):
+        div.attrib['ID'] = new_uuid()
+        if div.attrib['LABEL'].lower() == 'metadata':
+            try:
+                if div.attrib['DMDID'] in id_updates:
+                    div.attrib['DMDID'] = id_updates[div.attrib['DMDID']]
+                else:
+                    div.attrib['DMDID'] = new_uuid()
+            except KeyError:
+                print("Expecting 'DMDID' in structmap metadata")
+        for item in div:
+            if str(item.tag) == '{http://www.loc.gov/METS/}fptr':
+                if item.attrib['FILEID'] in id_updates:
+                    item.attrib['FILEID'] = id_updates[item.attrib['FILEID']]
+            elif str(item.tag) == '{http://www.loc.gov/METS/}mptr':
+                if item.attrib['{http://www.w3.org/1999/xlink}title'] in id_updates:
+                    item.attrib['{http://www.w3.org/1999/xlink}title'] = id_updates[
+                        item.attrib['{http://www.w3.org/1999/xlink}title']]
+            else:
+                print('Got unsupported pointer:', item.tag, "while updating mets ID")
+
+
+def create_root_aip_mets(sip_mets, aip_root, id_updates):
+
+    # register namespaces to ET parser
+    namespaces = {}
+    for key, value in ET.iterparse(sip_mets, events=['start-ns']):
+        ET.register_namespace(value[0], value[1])
+        namespaces[value[0]] = value[1]
+
+    tree = ET.parse(sip_mets)
+    root = tree.getroot()
+    created_now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f%z")
+
+    metshdr_elemet = root.find('{%s}metsHdr' % namespaces[''])
+    metshdr_elemet.attrib['LASTMODDATE'] = created_now
+    metshdr_elemet.attrib['RECORDSTATUS'] = 'Revised'
+    metshdr_elemet.attrib['{%s}OAISPACKAGETYPE' % namespaces['csip']] = 'AIP'
+
+    new_filegrp = ET.Element('{%s}fileGrp' % namespaces[''])
+    new_filegrp.attrib['ID'] = new_uuid()
+    new_filegrp.attrib['USE'] = 'Submission'
+
+    new_file = ET.Element('{%s}file' % namespaces[''])
+    new_file.attrib['ID'] = new_uuid()
+    new_file.attrib['MIMETYPE'] = str(mimetypes.guess_type(sip_mets)[0])
+    new_file.attrib['SIZE'] = str(sip_mets.stat().st_size)
+    new_file.attrib['CREATED'] = created_now
+    new_file.attrib['CHECKSUM'] = get_checksum(sip_mets)
+    new_file.attrib['CHECKSUMTYPE'] = 'SHA-256'
+
+    new_flocat = ET.Element('{%s}FLocat' % namespaces[''])
+    new_flocat.attrib['{%s}type' % namespaces['xlink']] = 'simple'
+    new_flocat.attrib['{%s}href' % namespaces['xlink']] = 'submission/METS.xml'
+    new_flocat.attrib['LOCTYPE'] = 'URL'
+
+    new_file.append(new_flocat)
+    new_filegrp.append(new_file)
+    filesec_element = root.find('{%s}fileSec' % namespaces[''])
+    filesec_element.append(new_filegrp)
+
+    # Adjust representations filegroup
+    for fileGrp in filesec_element.findall('{%s}fileGrp' % namespaces['']):
+        # if any(map(fileGrp.attrib['USE'].startswith, ('Representations', 'representations'))):
+        if fileGrp.attrib['USE'].lower().startswith('representations'):
+            rep_parts = Path(fileGrp.attrib['USE']).parts
+            rep_name = rep_parts[1].rstrip('0123456789')
+            rep_number = "{:02}.1".format(int(rep_parts[1][len(rep_name):]))
+            preservation_rep_path = "{}/{}{}".format('representations', rep_name, rep_number)
+            fileGrp.attrib['USE'] = preservation_rep_path
+
+            preservation_mets = aip_root / preservation_rep_path / 'METS.xml'
+            if preservation_mets.exists():
+                files = [x for x in fileGrp.findall('{%s}file' % namespaces[''])]
+                if len(files) == 1:
+                    file = files[0]
+                    flocat = file.find('{%s}FLocat' % namespaces[''])
+                    if file.attrib['MIMETYPE'] == 'text/xml' and str(
+                            flocat.attrib['{%s}href' % namespaces['xlink']]).endswith('METS.xml'):
+                        file.attrib['MIMETYPE'] = str(mimetypes.guess_type(preservation_mets)[0])
+                        file.attrib['SIZE'] = str(preservation_mets.stat().st_size)
+                        file.attrib['CREATED'] = created_now
+                        file.attrib['CHECKSUM'] = get_checksum(preservation_mets)
+                        file.attrib['CHECKSUMTYPE'] = 'SHA-256'
+
+                        flocat.attrib['{%s}href' % namespaces['xlink']] = preservation_rep_path + '/METS.xml'
+                else:
+                    print("More than one file in SIP representations fileGrp:", fileGrp.tag, fileGrp.attrib)
+                    print("Not currently supported")
+            else:
+                print('ERROR: Expected Preservation METS.xml')
+
+    structmap_element = root.find('{%s}structMap' % namespaces[''])
+    root_div_element = structmap_element.find('{%s}div' % namespaces[''])
+    root_div_element.attrib['ID'] = new_uuid()
+    for div in root_div_element.findall('{%s}div' % namespaces['']):
+        if div.attrib['LABEL'].lower().startswith('representations'):
+            rep_parts = Path(div.attrib['LABEL']).parts
+            rep_name = rep_parts[1].rstrip('0123456789')
+            rep_number = "{:02}.1".format(int(rep_parts[1][len(rep_name):]))
+            preservation_rep_path = "{}/{}{}".format('representations', rep_name, rep_number)
+            div.attrib['LABEL'] = preservation_rep_path
+            for pointer in div:
+                if str(pointer.tag) == '{%s}mptr' % namespaces['']:
+                    pointer.attrib['{%s}href' % namespaces['xlink']] = preservation_rep_path + '/METS.xml'
+
+    update_all_mets_ids(tree, id_updates)
+
+    ET.indent(tree, space='    ', level=0)
+    tree.write('%s/METS.xml' % aip_root, encoding='utf-8', xml_declaration=True)
 
 
 def copy_sip_to_aip(sip_path, aip_path):
@@ -112,7 +282,6 @@ def copy_sip_to_aip(sip_path, aip_path):
                 elif item_path.is_file():
                     # Will be METS.xml
                     shutil.copy2(item_path, destination_path)
-                    # update_mets(destination_path)
                 else:
                     print("%s not found" % item)
         else:
@@ -124,20 +293,18 @@ def copy_sip_to_aip(sip_path, aip_path):
         sys.exit(1)
 
 
-def update_sip_representations(aip_path):
+def create_aip_representations(aip_path):
     """
 
     :param Path aip_path:
     :return:
     """
     # update representations and create preservation directories
-    output_submission_representations_path = aip_path / 'submission' / "representations"
+    aip_submission_representations_path = aip_path / 'submission' / "representations"
 
-    print("Valid Rep Sequence:", validate_representations_sequence(output_submission_representations_path))
-
-    if validate_representations_sequence(output_submission_representations_path):
-        for rep in sorted(output_submission_representations_path.iterdir()):
-            rep = str(rep.stem)
+    if validate_representations_sequence(aip_submission_representations_path):
+        for rep in sorted(aip_submission_representations_path.iterdir()):
+            rep = rep.stem
             rep_name = rep.rstrip('0123456789')
             rep_number = int(rep[len(rep_name):])
 
@@ -146,27 +313,34 @@ def update_sip_representations(aip_path):
             (preservation_rep_path / "data").mkdir(parents=True)
             # create preservation rep METS.xml
             create_mets(preservation_rep_path)
-
-            # update mets
-            # rep_mets_file_path = os.path.join(output_submission_representations_path, rep, "METS.xml")
-            # if os.path.isfile(rep_mets_file_path):
-            #     update_mets(rep_mets_file_path)
-            # else:
-            #     print("%s METS.xml missing." % rep)
+    else:
+        print("Exiting.")
+        sys.exit(1)
 
 
-def transform(sip_path, aip_path):
-    aip_name = sip_path.stem
-    aip_path = aip_path / aip_name
+def transform_sip_to_aip(sip_path, aip_path):
+    if True:
+        sip_name = sip_path.stem
+        sip_uuid = sip_name[sip_name.index('uuid'):]
+        package_name = sip_name[:sip_name.index('uuid')]
+        aip_uuid = new_uuid()
+        aip_name = package_name + aip_uuid
+        aip_path = aip_path / aip_name
+        id_updates = {sip_uuid: aip_uuid}
+    else:
+        sip_name = sip_path.stem
+        aip_path = aip_path / sip_name
+        id_updates = {}
 
     overwrite_and_create_directory(aip_path)
 
     copy_sip_to_aip(sip_path, aip_path)
 
-    update_sip_representations(aip_path)
+    create_aip_representations(aip_path)
 
-    # create root METS.xml
-    create_mets(aip_path)
+    sip_mets = Path(sip_path / 'METS.xml')
+
+    create_root_aip_mets(sip_mets, aip_path, id_updates)
 
     # TODO:
     #  - Transfer archivematica AIP into preservation
@@ -179,44 +353,25 @@ def create_mets(path):
     :param Path path:
     :return:
     """
-    print("METS.xml written in: %s" % path)
     mets = metsrw.METSDocument()
     mets.append(create_fse(path, path))
     mets.write(str(path / "METS.xml"), pretty_print=True)
+    print("METS written in: %s" % path)
 
 
-# NOT USED
-def update_mets(mets_file):
-    # register namespaces to ET parser
-    for key, value in ET.iterparse(mets_file, events=['start-ns']):
-        # print("Key: ", value[0])
-        # print("Value: ", value[1])
-        # print()
-        ET.register_namespace(value[0], value[1])
-
-    tree = ET.parse(mets_file)
-    root = tree.getroot()
-
-    try:
-        if root.attrib['TYPE']:
-            root.attrib['TYPE'] = "AIP"
-    except KeyError:
-        print("ERROR: mets element: TYPE attribute missing")
-
-    mets_header = root.find('{http://www.loc.gov/METS/}metsHdr')
-    if mets_header is not None:
-        mets_header.set('RECORDSTATUS', 'Revised')
-        mets_header.set('LASTMODDATE', str(datetime.now().strftime("%Y-%m-%dT%H:%M:%S")))
-        mets_header.set('{https://dilcis.eu/XML/METS/CSIPExtensionMETS}OAISPACKAGETYPE', "AIP")
-
-    tree.write(mets_file)
+def get_checksum(file):
+    sha256_hash = hashlib.sha256()
+    with open(file, "rb") as f:
+        for byte_block in iter(lambda: f.read(4096), b""):
+            sha256_hash.update(byte_block)
+        return sha256_hash.hexdigest()
 
 
 def create_fse(current_path, aip_root_path):
     """
-    Recursive call
+    Recursive call for creating metsrw.FSEntry tree to be written to METS
+    :param Path current_path: keeps track of current
     :param Path aip_root_path:
-    :param Path current_path:
     :return FSEntry fse:
     """
     base_directory = current_path.stem
@@ -232,9 +387,10 @@ def create_fse(current_path, aip_root_path):
 
     if "METS.xml" in os.listdir(current_path):
         print("METS found: %s" % current_path)
+        mets_path = str(current_path / 'METS.xml')
         file_fse = metsrw.FSEntry(use=fse_use, label="METS.xml",
                                   path=str(relative_path / "METS.xml"), type="Item",
-                                  file_uuid=str(uuid.uuid4()))
+                                  file_uuid=str(uuid.uuid4()), checksum=get_checksum(mets_path), checksumtype='SHA-256')
         fse.children.append(file_fse)
         return fse
 
@@ -243,9 +399,9 @@ def create_fse(current_path, aip_root_path):
         if item_path.is_dir():
             fse.children.append(create_fse(item_path, aip_root_path))
         elif item_path.is_file():
-            print("item path: ", item_path, fse_use)
             file_fse = metsrw.FSEntry(use=fse_use, label=item, path=str(relative_path / item), type="Item",
-                                      file_uuid=str(uuid.uuid4()))
+                                      file_uuid=str(uuid.uuid4()), checksum=get_checksum(item_path),
+                                      checksumtype='SHA-256')
             fse.children.append(file_fse)
         else:
             print("File Error - create_root_mets()")
@@ -253,14 +409,9 @@ def create_fse(current_path, aip_root_path):
 
 
 if __name__ == '__main__':
-    numArgs = len(sys.argv)
-
-    if numArgs == 3:
-        sip_directory = Path(get_arg(1))
-        output_directory = Path(get_arg(2))
-        if validate_directories(sip_directory, output_directory):
-            # TODO: Validate SIP folder structure?
-            transform(sip_directory, output_directory)
+    if len(sys.argv) == 3:
+        if validate_directories(Path(get_arg(1)), Path(get_arg(2))):
+            transform_sip_to_aip(Path(get_arg(1)), Path(get_arg(2)))
         else:
             sys.exit(1)
     else:
